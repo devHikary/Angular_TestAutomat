0d5b1e55f7a30e84027904c03d79e969de542bca
devHikary <wrkkarina@gmail.com>
2021-07-22 21:06:44 -0300

    Aula 2.2

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -4,7 +4,14 @@ describe(UniqueIdService.name, () => {
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
-    expect(id).toContain('app-');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should notgerenate duplicate IDs when called multiple times`, () => {
+    const service = new UniqueIdService();
+    const firstId = service.generateUniqueIdWithPrefix('app');
+    const secondId = service.generateUniqueIdWithPrefix('app');
+    expect(firstId).not.toBe(secondId);
   });
 });
 
